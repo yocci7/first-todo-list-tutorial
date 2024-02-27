a3ab569a1f43b163b3af76f8098f591528b71545
yocci_ii <hiitoy0117@gmail.com>
2024-02-28 05:31:43 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react';
+import TodoList from './TodoList';
+import {v4 as uuidv4} from "uuid";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const todoNameRaf = useRef();
+
+  const handleAddTodo = () => {
+    const name = todoNameRaf .current.value;
+    if(name === "") return
+    setTodos((prevTodos) => {
+      return [...prevTodos, {id: uuidv4(), name: name, completed: false}]
+    });
+    todoNameRaf.current.value = null;
+  }
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos]
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  }
+
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed)
+    setTodos(newTodos)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type='text' ref={todoNameRaf} />
+      <button onClick={handleAddTodo}>Add task</button>
+      <button onClick={handleClear}>Delete Task</button>
+      <div>Remaining tasks:{todos.filter((todo) => !todo.completed).length}</div>
     </div>
   );
 }
